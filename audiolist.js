a63c4c9533502bdf06b439ea89f4d52477d5a8f4
m4cey <h4mmer7ime@gmail.com>
2023-12-25 21:41:43 +0100

    disco!

--- audiolist.js
+++ audiolist.js
@@ -1,1 +1,1 @@
-audiolist = [["audio0.mp3", ],["audio1.mp3", ],["audio2.mp3", ],["audio3.mp3", ],["audio4.mp3", ],["audio5.mp3", ],["audio6.mp3", ],["audio7.mp3", "audio7b.mp3", "audio7c.mp3", ],["audio8.mp3", "audio8b.mp3", ],["audio9.mp3", ],]
+audiolist = [["audio0.mp3", ],["audio1.mp3", ],["audio2.mp3", ],["audio3.mp3", ],["audio4.mp3", ],["audio5.mp3", ],["audio6disco.mp3", "audio6.mp3", ],["audio7b.mp3", "audio7c.mp3", "audio7.mp3", ],["audio8b.mp3", "audio8.mp3", ],["audio9.mp3", ],]
